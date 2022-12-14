94c8013c923c0890cfb7341634dec26911c30c0f
譚凱祥 <htankhaishan@gmail.com>
2022-12-14 20:40:58 +0800

    Initial commit

---
+++ Excellent Tuition Centre (ETC)/Form1.cs
@@ -0,0 +1,10 @@
+namespace Excellent_Tuition_Centre__ETC_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
