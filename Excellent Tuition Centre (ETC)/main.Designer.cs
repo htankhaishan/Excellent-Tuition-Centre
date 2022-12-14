99a0719a530c3ace3fda0942a72059178a6e914d
譚凱祥 <htankhaishan@gmail.com>
2022-12-14 22:30:07 +0800

    main_menu_testing

--- Excellent Tuition Centre (ETC)/main.Designer.cs
+++ Excellent Tuition Centre (ETC)/main.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.test1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // test1
+            // 
+            this.test1.Location = new System.Drawing.Point(192, 86);
+            this.test1.Name = "test1";
+            this.test1.Size = new System.Drawing.Size(75, 23);
+            this.test1.TabIndex = 0;
+            this.test1.Text = "Click me";
+            this.test1.UseVisualStyleBackColor = true;
+            this.test1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(473, 216);
+            this.Controls.Add(this.test1);
+            this.Name = "main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button test1;
     }
 }
--- Excellent Tuition Centre (ETC)/main.cs
+++ Excellent Tuition Centre (ETC)/main.cs
@@ -6,5 +6,10 @@ namespace Excellent_Tuition_Centre__ETC_
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you!");
+        }
     }
 }
