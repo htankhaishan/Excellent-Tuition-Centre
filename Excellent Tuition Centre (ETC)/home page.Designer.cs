da77fa47c4e6df58dae614fabb3b960d11058b83
LIM WEI HONG <TP068463@mail.apu.edu.my>
2023-01-13 15:44:56 +0800

    Added logo removed rice was here

--- Excellent Tuition Centre (ETC)/home page.Designer.cs
+++ Excellent Tuition Centre (ETC)/home page.Designer.cs
@@ -31,11 +31,13 @@
             this.test1 = new System.Windows.Forms.Button();
             this.lblRice = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.picbLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picbLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // test1
             // 
-            this.test1.Location = new System.Drawing.Point(353, 265);
+            this.test1.Location = new System.Drawing.Point(524, 333);
             this.test1.Name = "test1";
             this.test1.Size = new System.Drawing.Size(75, 23);
             this.test1.TabIndex = 0;
@@ -46,11 +48,10 @@
             // lblRice
             // 
             this.lblRice.AutoSize = true;
-            this.lblRice.Location = new System.Drawing.Point(167, 91);
+            this.lblRice.Location = new System.Drawing.Point(202, 129);
             this.lblRice.Name = "lblRice";
-            this.lblRice.Size = new System.Drawing.Size(78, 15);
+            this.lblRice.Size = new System.Drawing.Size(0, 15);
             this.lblRice.TabIndex = 1;
-            this.lblRice.Text = "Rice was here";
             // 
             // button1
             // 
@@ -62,18 +63,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_2);
             // 
+            // picbLogo
+            // 
+            this.picbLogo.BackgroundImage = global::Excellent_Tuition_Centre__ETC_.Properties.Resources.Logo;
+            this.picbLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picbLogo.InitialImage = null;
+            this.picbLogo.Location = new System.Drawing.Point(69, 77);
+            this.picbLogo.Name = "picbLogo";
+            this.picbLogo.Size = new System.Drawing.Size(335, 269);
+            this.picbLogo.TabIndex = 3;
+            this.picbLogo.TabStop = false;
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(804, 445);
+            this.Controls.Add(this.picbLogo);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lblRice);
             this.Controls.Add(this.test1);
             this.Name = "main";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picbLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,5 +98,6 @@
         private Button test1;
         private Label lblRice;
         private Button button1;
+        private PictureBox picbLogo;
     }
 }
